8a9c61f2536de6260aea121504bde485f532906a
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-01-14 22:10:35 +0530

    update README.md

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__."/vendor/autoload.php";
+
+use App\Hurricane\Hurricane;
+
+//$app = new Hurricane();
+//$app->Server("localhost"),
+      // ->Listen(8000);
+//dump($run);
+//
+$arr = [1,2,3,4];
+dump($arr);
+$var = [
+    'a simple string' => "in an array of 5 elements",
+    'a float' => 1.0,
+    'an integer' => 1,
+    'a boolean' => true,
+    'an array' => ['cat',3,true,5.7],
+];
+dump($var);
--- src/Hurricane.php
+++ src/Hurricane.php
@@ -1,12 +1,23 @@
 <?php
-namespace Hurricane\Hurricane;
+namespace App\Hurricane;
 
-class Hurricane{
-  public static function App(){
+class Hurricane
+{
+  private string $server;
+  private int $port;
 
+  public function __construct()
+  {
+    echo "php -S {$this->server}:{$this->port}";
   }
 
-  public static function Serve(){
+  public function Server(?string $server = null)
+  {
+    return $this->server = $server;
+  }
 
+  public function Listen(?int $port = null)
+  {
+    return $this->port = $port;
   }
 }
